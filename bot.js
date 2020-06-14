0cbba7150c7fd01a9c7b42993ce054fdd4fbdd53
Jose Manuel <josemanuelc801@gmail.com>
2020-06-14 11:58:49 -0300

    Se codifica bot.js para que respondo lo que se envia

--- bot.js
+++ bot.js
@@ -2,6 +2,10 @@ const telegraft = require('telegraf')
 
 const bot = new telegraft('1284270460:AAHXVdqFvlIVBWQkrT5gTg7fx1CuWwRfuhE')
 
-bot.start((ctx) => ctx.reply('Welcome!'))
+bot.start((ctx) => ctx.reply('Hola'))
+
+bot.on('text',(ctx) => {
+    ctx.reply(ctx.update.message.text)
+})
 
 bot.launch()
