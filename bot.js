4860aacf55f9a38b2cf4cdf56148dae47a533344
Jose Manuel <josemanuelc801@gmail.com>
2020-09-25 21:24:02 -0300

    Modificacion impuestos steam /imp {numero}

--- bot.js
+++ bot.js
@@ -2,10 +2,36 @@ const telegraft = require('telegraf')
 
 const bot = new telegraft('1284270460:AAHXVdqFvlIVBWQkrT5gTg7fx1CuWwRfuhE')
 
+const command1 = '/imp'
+
 bot.start((ctx) => ctx.reply('Hola'))
 
-bot.on('text',(ctx) => {
-    ctx.reply(ctx.update.message.text)
+
+function calculaImpuesto (valor){
+
+    if(parseFloat(valor)){
+
+        valorint = parseFloat(valor);
+
+        valor8 =  valorint * 0.08;
+
+        valor21 = valorint * 0.21;
+
+        valor = valorint + valor8 + valor21
+
+        return 'Valor original : '+ valorint +'$\n21% IVA : '+ valor21 + '$\n8% de nose : '+ valor8 + '$\nTotal : ' + valor + '$';    
+    }
+        return 'Se debe ingresar un nuemro'
+
+}
+
+
+
+bot.command(command1,(ctx) =>{
+
+    let msj = (ctx.message.text).substring(command1.length+1,ctx.message.text.length);
+    ctx.reply(calculaImpuesto(msj));
+
 })
 
 bot.launch()
